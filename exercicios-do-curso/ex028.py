0086342355be96640a7e1c81fa50f49b369ea974
Caio Marcelo Olivi <caioolivi18@gmail.com>
2024-03-20 21:53:39 -0400

    Coloquei cores no exercicio 28

--- exercicios-do-curso/ex028.py
+++ exercicios-do-curso/ex028.py
@@ -3,16 +3,31 @@
 # pelo computador. O programa deverá escrever na tela se o usuário venceu
 # ou perdeu.
 
-import random 
-import time
-print('-=-' * 25)
-print('Pensei em um número entre 0 e 5. Você consegue adivinhar em qual pensei?')
-print('-=-' * 25)
-numero = random.randint(0, 5)
-numusu = int(input('Digite qual você acha: '))
-print('VERIFICANDO...')
-time.sleep(2)
+from colorama import Fore, init
+from random import randint 
+from time import sleep
+
+init()
+
+color = {
+    'yellow': Fore.YELLOW,
+    'red': Fore.RED,
+    'blue': Fore.BLUE,
+    'magenta': Fore.MAGENTA,
+    'green': Fore.GREEN,
+    'reset':Fore.RESET,
+}
+
+division = (color['yellow'] + '-=-' + color['reset']) * 25
+
+print(division)
+print(f"{color['blue']} Pensei em um número entre 0 e 5. Você consegue adivinhar em qual pensei?")
+print(division)
+numero = randint(0, 5)
+numusu = int(input(f"\n {color['blue']} Digite qual você acha: {color['reset']}"))
+print(f"\n {color['magenta']} VERIFICANDO...{color['reset']}")
+sleep(2)
 if numero == numusu:
-    print(f'Você acertou! Número do computador: {numero}')
+    print(f"\n {color['green']} Você acertou!{color['reset']} \n Número do computador: {numero}")
 else:
-    print(f'Você ERROU! Número do computador: {numero}')
+    print(f"\n {color['red']} Você ERROU!{color['reset']} \n Número do computador: {numero}")
