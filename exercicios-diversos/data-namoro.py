1ae6e504e28ba41bba28e3e7540d5f965f479e65
Caio Marcelo Olivi <caioolivi18@gmail.com>
2024-04-02 15:56:26 -0400

    Criado o data-namoro

--- exercicios-diversos/data-namoro.py
+++ exercicios-diversos/data-namoro.py
@@ -1,11 +1,30 @@
+
+import customtkinter
 from datetime import *
 
-data_pedido_namoro = datetime(2023, 10, 29)
-data_hoje = datetime.today()
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
-diferença = data_hoje - data_pedido_namoro
+janela = customtkinter.CTk()
+janela.geometry("600x400")
 
-print('========== Tempo de namoro. ==========')
-print(f'{diferença.days} dias.')
-print(f'{diferença.days / 30:.0f} meses.')
+data_namoro = datetime(2023, 10, 29)
+data_atual = datetime.today()
+diferença = data_atual - data_namoro
 
+meses_completos = diferença.days // 30
+
+texto = customtkinter.CTkLabel(janela, text="RELANTIONSHIP SAVER 🤫")
+texto.pack(padx=5, pady=5)
+
+dt_namoro = customtkinter.CTkLabel(janela, text="Data do namoro: 29/10/2023")
+dt_namoro.pack(padx=5, pady=5)
+
+dias = customtkinter.CTkLabel(janela, text=f"Dias: {diferença.days}")
+dias.pack(padx=5, pady=5)
+
+meses = customtkinter.CTkLabel(janela, text=f"Meses: {meses_completos}")
+meses.pack(padx=5, pady=5)
+
+
+janela.mainloop()
